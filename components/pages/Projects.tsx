4166f29832f6f301b27534b63202fc1044be5cba
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-01-24 20:57:04 +0300

    complete projects sections

--- components/pages/Projects.tsx
+++ components/pages/Projects.tsx
@@ -1,18 +1,36 @@
 import { Sections } from "@/enums/global";
 import ProjectContainer from "../ui/projects/ProjectContainer";
-import audioGuide from '@/assets/images/projects/AudioGuide.png'
-import zaoStore from '@/assets/images/projects/zaoStore.png'
-import EyeClinic from '@/assets/images/projects/EyeClinic.png'
+import audioGuide from "@/assets/images/projects/AudioGuide.png";
+import zaoStore from "@/assets/images/projects/zaoStore.png";
+import EyeClinic from "@/assets/images/projects/EyeClinic.png";
 const Projects = ({ projectsSectionRef }: any) => {
   return (
     <section
       id={Sections.PROJECTS_SECTION}
-      className="projects  "
+      className="projects "
       ref={projectsSectionRef}
     >
-      <ProjectContainer image={EyeClinic} />
-      <ProjectContainer image={zaoStore} />
-      <ProjectContainer image={audioGuide}/>
+      <div className="containerTitleProject">
+        <h1 className="">My project</h1>
+      </div>
+      <ProjectContainer
+        image={EyeClinic}
+        title="Eye Clinic"
+        href="https://eye-clinic-77ea0.firebaseapp.com"
+        documenting="I completed a web application for an eye clinic as the final project for the 'Masart' course sponsored by the United States Unity (USAID). Developed using React and Tailwind, the frontend offers a modern and responsive user interface. Firebase API was integrated for secure data management, allowing real-time updates. This project reflects my proficiency in web development and practical application of skills gained from the 'Masart' course."
+      />
+      <ProjectContainer
+        image={zaoStore}
+        title="Zao Store"
+        href='https://zao-store-e7fe8.web.app/'
+        documenting=" Freelance project: Crafted a dynamic social media store to meet the specific needs of the client, incorporating unique custom features. Implemented real API integration for efficient communication with the backend, enhancing the user experience. The solution not only reflects my adaptability to custom requirements but also showcases a commitment to delivering a tailored and high-quality product."
+      />
+      <ProjectContainer
+        image={audioGuide}
+        title="Audio Guide"
+        href='https://xmustafa5.github.io/endd/'
+        documenting="Graduation project from the University of Technology: Designed an innovative audio guide exclusively using vanilla JavaScript. This project, focused on guiding visitors through the Computer Science department, exemplifies my proficiency in creating efficient solutions without external APIs. The audio guide provides a seamless and user-friendly experience, demonstrating my dedication to delivering practical applications in the field of technology."
+      />
     </section>
   );
 };
--- components/ui/projects/ProjectContainer.tsx
+++ components/ui/projects/ProjectContainer.tsx
@@ -1,23 +1,33 @@
-import  Image from 'next/image';
-import zaoStore from '@/assets/images/projects/AudioGuide.png'
-const ProjectContainer = ({image}:any) => {
-    return (
-      <div className="projectContainer">
+import Image from "next/image";
+import zaoStore from "@/assets/images/projects/AudioGuide.png";
+const ProjectContainer = ({ image, title, documenting, href }: any) => {
+  return (
+    <div className="pb-6">
+    <div className="projectContainer ">
         <div className="rightProject">
-            <div className='imageProjectContainer'>
-
-            <Image src={image} alt='project Image' className='imageProject'/>
-            </div>
+          <div className="imageProjectContainer">
+            <Image src={image} alt="project Image" className="imageProject" />
+          </div>
         </div>
         <div className="leftContainer">
-            <h1>Eye Clinc</h1>
-            <p>Developed a web application for an eye clinic as the final project for the ""masart provided by the Unite States Unity (USAID)" course</p>
+          <div className="leftProjectContainer ">
+            <h1>{title}</h1>
+            <p>{documenting}</p>
             <div className="containerButtonProject">
-                <button className="downloadCvButton">view More</button>
+              <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="downloadCvButton"
+              >
+                View More
+              </a>
             </div>
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default ProjectContainer;
+      </div>
+  );
+};
+
+export default ProjectContainer;
